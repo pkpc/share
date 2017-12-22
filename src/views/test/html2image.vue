d67d9dab57885a0838348bdd1c823c2cfe476ba4
pkpc <18070567389@163.com>
2017-12-22 11:07:49 +0800

    '测试有develop分支直接rebase回master分支'
    '搞事情'
    
    'fix测试1.0'
    
    'fix测试1.1'
    
    '这是一个新的develop分支，并进行了修改'
    
    '修改develop分支'
    
    '再次修改'

--- src/views/test/html2image.vue
+++ src/views/test/html2image.vue
@@ -7,11 +7,13 @@
   export default {
 
     data(){
-
       return{
-
+        dev0:0,
+        dev1:1,
+        dev2:2,
+        developData:[],
+        developCreated2:[],
         lpi:{},
-
         yes:[],
         dkkk:[]
       }
